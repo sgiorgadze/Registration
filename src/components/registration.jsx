108daf7115e2aefb5b9ae38760ed3528b0827953
salome1giorgadze <salome1giorgadze@gmail.com>
2024-03-15 23:38:27 +0400

    add messages

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -29,37 +29,8 @@ class Registration extends Component {
     },
 
     days: [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-      21,
-      22,
-      23,
-      24,
-      25,
-      26,
-      27,
-      28,
-      29,
-      30,
-      31,
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
     ],
     months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     years: [],
@@ -213,15 +184,8 @@ class Registration extends Component {
   };
 
   render() {
-    const {
-      account,
-      errors,
-      days,
-      months,
-      years,
-      countries,
-      defaultCountry,
-    } = this.state;
+    const { account, errors, days, months, years, countries, defaultCountry } =
+      this.state;
     return (
       <div className="wrapper">
         <div className="register__form">
